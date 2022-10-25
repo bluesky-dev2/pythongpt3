1504ddfd5a250019ec2e0f37fdd8fa3901096069
Devan Nair <alifeair.source@gmail.com>
2022-10-25 22:53:43 +0800

    initial commit

--- chat.py
+++ chat.py
@@ -9,7 +9,7 @@ def open_file(filepath):
 openai.api_key = open_file('openaiapikey.txt')
 
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['JAX:', 'USER:']):
+def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['STELLA:', 'USER:']):
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
     response = openai.Completion.create(
         engine=engine,
@@ -31,7 +31,7 @@ if __name__ == '__main__':
         conversation.append('USER: %s' % user_input)
         text_block = '\n'.join(conversation)
         prompt = open_file('prompt_chat.txt').replace('<<BLOCK>>', text_block)
-        prompt = prompt + '\nJAX:'
+        prompt = prompt + '\nSTELLA:'
         response = gpt3_completion(prompt)
-        print('JAX:', response)
-        conversation.append('JAX: %s' % response)
+        print('STELLA:', response)
+        conversation.append('STELLA: %s' % response)
